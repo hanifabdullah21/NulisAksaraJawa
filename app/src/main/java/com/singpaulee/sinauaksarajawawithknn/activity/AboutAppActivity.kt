3d7371c0c9e9e094351c2c4d5fe7382b3e1fc641
singpaulee <hanifabdullah7@gmail.com>
2019-07-31 01:47:31 +0700

    Delete profil

--- app/src/main/java/com/singpaulee/sinauaksarajawawithknn/activity/AboutAppActivity.kt
+++ app/src/main/java/com/singpaulee/sinauaksarajawawithknn/activity/AboutAppActivity.kt
@@ -3,11 +3,18 @@ package com.singpaulee.sinauaksarajawawithknn.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.singpaulee.sinauaksarajawawithknn.R
+import kotlinx.android.synthetic.main.activity_about_app.*
+import org.jetbrains.anko.intentFor
+import org.jetbrains.anko.sdk27.coroutines.onClick
 
 class AboutAppActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about_app)
+
+        aaa_tv_title.onClick {
+            startActivity(intentFor<TrainingResultActivity>())
+        }
     }
 }
